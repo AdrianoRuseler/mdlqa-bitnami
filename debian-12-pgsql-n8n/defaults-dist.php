a96669ac923ddf272121a0a643b55643b2737024
Adriano Ruseler <adrianoruseler@gmail.com>
2025-06-18 14:01:30 -0300

    Added n8n option

---
+++ debian-12-pgsql-n8n/defaults-dist.php
@@ -0,0 +1,24 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle QA Site! '; // for core settings
+$defaults['moodle']['custommenuitems'] = 'Theme
+-Boost | ?theme=boost
+-Classic | ?theme=classic
+';
+$defaults['moodle']['allowthemechangeonurl'] = 1; 
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+$defaults['moodle']['country'] = 'Brazil';
+
+$defaults['moodle']['pathtophp'] = '/opt/bitnami/php/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
+$defaults['moodle']['pathtopdftoppm'] = '/usr/bin/pdftoppm';
+$defaults['moodle']['pathtopython'] = '/usr/bin/python3';
+
+$defaults['moodle']['forcelogin'] = 1;
+
+$defaults['moodle']['auth_instructions'] = 'MOODLE_USERNAME: admin
+        MOODLE_PASSWORD: 97XmKiL)*3';
+        
